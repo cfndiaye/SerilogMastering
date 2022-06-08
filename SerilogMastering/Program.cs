b21943d242d9b76426cb643b03df32aad047a12f
cfndiaye <cfndiaye@outlook.com>
2022-06-08 12:54:28 +0000

    add project

---
+++ SerilogMastering/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Hosting;
+
+using Serilog;
+using Serilog.AspNetCore;
+
+namespace SerilogMastering
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            //
+
+            //read configuration from file
+            var config = new ConfigurationBuilder().AddJsonFile("appsettings.json").Build();
+
+            //Initialize logger
+            Log.Logger = new LoggerConfiguration().ReadFrom.Configuration(config).CreateLogger();
+            try
+            {
+                Log.Information("Application Starting");
+                CreateHostBuilder(args).Build().Run();
+            }
+            catch(Exception ex)
+            {
+                Log.Fatal(ex, "Application failed to start.");
+            }
+            finally
+            {
+                Log.CloseAndFlush();
+            }
+        }
+
+        public static IHostBuilder CreateHostBuilder(string[] args) =>
+            Host.CreateDefaultBuilder(args)
+                .UseSerilog() //Use Serilog instead of default .Net logger
+                .ConfigureWebHostDefaults(webBuilder =>
+                {
+                    webBuilder.UseStartup<Startup>();
+                });
+    }
+}
